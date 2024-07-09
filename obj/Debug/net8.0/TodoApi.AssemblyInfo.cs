8aefe1a93c77ee7e78ea964f640f31ae29be05c8
Moana <sabiu7108@gmail.com>
2024-07-09 23:55:39 +0900

    Adding Context and model for item

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+241027d9f4743475df8fa1931051de4783c85928")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5d42095ae703a939ff22d9ba02d700e10f4d9a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
